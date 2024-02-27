7a243b80de835ba43de9db5fc8287fed34c077f6
michaelchristwin <kelechukwuchristwin@gmail.com>
2024-02-27 09:07:50 +0100

    Added lazy loading to react-player

--- src/app/page.js
+++ src/app/page.js
@@ -13,9 +13,8 @@ import {
 
 import ReactGA from "react-ga4";
 import toPng from "../../utils/toPng";
-import ReactPlayer from "react-player";
-//const ReactPlayer = dynamic(import("react-player"));
 import dynamic from "next/dynamic";
+const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
 import { m3terAlias, m3terAttributes, M3terHead } from "m3ters";
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
